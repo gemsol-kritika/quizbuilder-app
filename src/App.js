981c9d0dbbaebe532388fe553a6c80a2018b4c34
gemsol-kritika <kritika.sharma@geminisolutions.com>
2022-05-23 21:16:50 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Signup';
+import Signup from './components/Signup';
 
 function App() {
 
 return (
 	<Router>
 	<div className="App">
-	<Route exact path='/register' component={Register} />
+	<Route exact path='/register' component={Signup} />
 	<Route exact path='/login' component={Login} />
 	</div>
 	</Router>
